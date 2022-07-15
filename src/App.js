20d74dc9095dd4329a0796be4fb6d896471609c1
chf <943154022@11.com>
2022-07-15 10:44:11 +0800

    盘面精细化：设置宫格组件，并划分九宫格

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import { useState } from 'react';
 import './App.css';
+import Square from "./components/square"
 
 function App() {
   const [board,setBoard] =useState(["","","","","","","","","",""]);
-
+  
+  const selectSquare=(square)=>{
+     
+  }
   return (
     <div className="App">
       <div className='board'>
-        
+        <div className='row'>
+          <Square val={board[0]}
+           selectSquare={()=>{selectSquare(0)}}/>
+           <Square val={board[1]}
+           selectSquare={()=>{selectSquare(1)}}/>
+           <Square val={board[2]}
+           selectSquare={()=>{selectSquare(2)}}/>
+        </div>
+        <div className='row'>
+        <Square val={board[3]}
+           selectSquare={()=>{selectSquare(3)}}/>
+           <Square val={board[4]}
+           selectSquare={()=>{selectSquare(4)}}/>
+           <Square val={board[5]}
+           selectSquare={()=>{selectSquare(5)}}/>
+        </div>
+        <div className='row'>
+        <Square val={board[6]}
+           selectSquare={()=>{selectSquare(6)}}/>
+           <Square val={board[7]}
+           selectSquare={()=>{selectSquare(7)}}/>
+           <Square val={board[8]}
+           selectSquare={()=>{selectSquare(8)}}/>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/square.js
@@ -0,0 +1,13 @@
+import React from "react";
+import App from "../App";
+import "../App.css"
+function Square({val,selectSquare}){
+    return (
+        <div className="square" onClick={selectSquare}>
+            {val}
+
+        </div>
+    )
+}
+
+export default Square;
