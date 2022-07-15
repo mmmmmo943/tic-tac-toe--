71d2a1a79bb2560bc8dfec689be30cebd630cea6
chf <943154022@11.com>
2022-07-15 10:18:55 +0800

    游戏盘面搭建

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [board,setBoard] =useState(["","","","","","","","","",""]);
+
   return (
     <div className="App">
-      
+      <div className='board'>
+        
+      </div>
     </div>
   );
 }
